ca70ea5dcbda354ba54cb347ba36de4835d54a1e
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-02-26 09:58:33 +0100

    chande feault state to game for now

--- src/global_state.rs
+++ src/global_state.rs
@@ -2,7 +2,7 @@ use bevy::ecs::schedule::States;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 pub enum SceneState {
-    #[default]
     Menu,
+    #[default]
     Game,
 }
