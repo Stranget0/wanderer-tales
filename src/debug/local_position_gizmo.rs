be9e9f31f56b9699b6e3fb9b3c1fc9b97c368745
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-03-28 17:54:58 +0100

    make debug transforms go from source to rendered item

--- src/debug/local_position_gizmo.rs
+++ src/debug/local_position_gizmo.rs
@@ -1,38 +1,48 @@
 use bevy::prelude::*;
 
+use crate::gameplay::map::renderer::renderers::RenderMapApi;
 use crate::gameplay::map::renderer::state::RendererState;
 use crate::utils::*;
 
 #[derive(Component)]
-pub struct LocalGizmo;
+pub struct LocalGizmoSource;
 
-pub fn draw_local_gizmos(
+pub fn draw_local_gizmos<R: Component + RenderMapApi>(
     mut gizmos: Gizmos,
-    player: Query<&Transform, With<LocalGizmo>>,
-    renderer: Res<State<RendererState>>,
+    targets_query: Query<Entity, With<LocalGizmoSource>>,
+    transform_query: Query<&Transform>,
+    renderer_state: Res<State<RendererState>>,
+    renderer_query: Query<&R>,
 ) {
-    for t in player.iter() {
-        let multiplier = match renderer.get() {
-            RendererState::TwoDimension => {
-                continue;
+    for source_entity in targets_query.iter() {
+        for renderer in renderer_query.iter() {
+            if let Some(t) = renderer
+                .get_render_item(&source_entity)
+                .and_then(|render_entity| transform_query.get(*render_entity).ok())
+            {
+                let multiplier = match renderer_state.get() {
+                    RendererState::TwoDimension => {
+                        continue;
+                    }
+                    _ => 1.0,
+                };
+                let offset = Vec3::from_array(t.translation.to_array());
+                gizmos.arrow(
+                    offset,
+                    offset + t.rotation.mul_vec3(Vec3::X) * multiplier,
+                    Color::RED,
+                );
+                gizmos.arrow(
+                    offset,
+                    offset + t.rotation.mul_vec3(FORWARD) * multiplier,
+                    Color::GREEN,
+                );
+                gizmos.arrow(
+                    offset,
+                    offset + t.rotation.mul_vec3(UP) * multiplier,
+                    Color::BLUE,
+                );
             }
-            _ => 1.0,
-        };
-        let offset = Vec3::from_array(t.translation.to_array());
-        gizmos.arrow(
-            offset,
-            offset + t.rotation.mul_vec3(Vec3::X) * multiplier,
-            Color::RED,
-        );
-        gizmos.arrow(
-            offset,
-            offset + t.rotation.mul_vec3(FORWARD) * multiplier,
-            Color::GREEN,
-        );
-        gizmos.arrow(
-            offset,
-            offset + t.rotation.mul_vec3(UP) * multiplier,
-            Color::BLUE,
-        );
+        }
     }
 }
