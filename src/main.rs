ea497e5a099f504cbbba2fc5d3bcf9ec85339a99
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-04-26 17:10:01 +0200

    mesh generation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use bevy::{
     pbr::wireframe::WireframeConfig,
     prelude::*,
     render::{
+        extract_resource::ExtractResource,
         settings::{RenderCreation, WgpuFeatures, WgpuSettings},
         RenderPlugin,
     },
@@ -46,9 +47,14 @@ fn main() {
             // Can be changed per mesh using the `WireframeColor` component.
             default_color: Color::YELLOW_GREEN,
         })
-        .insert_resource(ChunkViewDistance(5))
         .insert_resource(MaxHeight(3))
+        .insert_resource(LODSetter {
+            max_size: 100.0,
+            min_size: 10.0,
+            view_distance: 1000,
+        })
         .insert_resource(MapLOD::new())
+        .register_type::<LODSetter>()
         .add_systems(Startup, (render_chunks))
         .add_systems(Update, (draw_gizmos))
         .run();
@@ -81,6 +87,10 @@ impl<const SIZE: usize, T> FixedTreeNode<SIZE, T> {
         FixedTreeNodeIterator { stack: vec![self] }
     }
 
+    pub fn iter_mut(&mut self) -> FixedTreeNodeIteratorMut<'_, SIZE, T> {
+        FixedTreeNodeIteratorMut { stack: vec![self] }
+    }
+
     pub fn update_with_setter<S: TreeSetter<TreeNode = Self>>(&mut self, setter: &S) {
         setter.handle_node(self);
     }
@@ -88,6 +98,9 @@ impl<const SIZE: usize, T> FixedTreeNode<SIZE, T> {
 
 struct FixedTreeNodeIterator<'a, const SIZE: usize, T> {
     stack: Vec<&'a FixedTreeNode<SIZE, T>>,
+}
+struct FixedTreeNodeIteratorMut<'a, const SIZE: usize, T> {
+    stack: Vec<&'a mut FixedTreeNode<SIZE, T>>,
 }
 
 impl<'a, const SIZE: usize, T> Iterator for FixedTreeNodeIterator<'a, SIZE, T> {
@@ -104,6 +117,28 @@ impl<'a, const SIZE: usize, T> Iterator for FixedTreeNodeIterator<'a, SIZE, T> {
                 FixedTreeNode::Data(ref data) => return Some(data),
                 FixedTreeNode::Split(ref children) => {
                     for child in children.iter() {
+                        self.stack.push(child);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'a, const SIZE: usize, T> Iterator for FixedTreeNodeIteratorMut<'a, SIZE, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let node = match self.stack.pop() {
+                Some(n) => n,
+                None => return None,
+            };
+
+            match node {
+                FixedTreeNode::Data(ref mut data) => return Some(data),
+                FixedTreeNode::Split(ref mut children) => {
+                    for child in children.iter_mut() {
                         self.stack.push(child);
                     }
                 }
@@ -153,28 +188,32 @@ impl MapLOD {
     }
 }
 
+#[derive(Resource, Debug, Clone, Default, ExtractResource, Reflect)]
+#[reflect(Resource)]
 struct LODSetter {
     pub view_distance: u16,
-    pub ref_distance: u16,
-    pub min_precision: u16,
-    pub ref_precision: u16,
-    pub max_depth: usize,
+    pub min_size: f32,
+    pub max_size: f32,
 }
 
 impl LODSetter {
-    // Function to convert distance to precision level
-    pub fn distance_to_precision(&self, distance: f32) -> u16 {
-        let t = (distance / self.ref_distance as f32).clamp(0.0, 1.0);
-        let precision_range = (self.ref_precision - self.min_precision) as f32;
-        let precision = (1.0 - t) * precision_range + self.min_precision as f32;
-        precision.round() as u16
-    }
-    // Function to determine if a node should be subdivided based on current precision and distance
-    pub fn should_subdivide(&self, current_precision: u16, distance: f32) -> bool {
-        let target_precision = self.distance_to_precision(distance);
-        current_precision < target_precision
-    }
+    // // Function to convert distance to precision level
+    // pub fn distance_to_precision(&self, distance: f32) -> u16 {
+    //     let t = (distance / self.ref_distance as f32).clamp(0.0, 1.0);
+    //     let precision_range = (self.ref_precision - self.min_precision) as f32;
+    //     let precision = (1.0 - t) * precision_range + self.min_precision as f32;
+    //     precision.round() as u16
+    // }
+    // // Function to determine if a node should be subdivided based on current precision and distance
+    // pub fn should_subdivide(&self, current_precision: u16, distance: f32) -> bool {
+    //     let target_precision = self.distance_to_precision(distance);
+    //     current_precision < target_precision
+    // }
 
+    pub fn distance_to_size(&self, distance: f32) -> f32 {
+        let factor = distance / self.view_distance as f32;
+        self.min_size.lerp(self.max_size, factor)
+    }
     // Function to get size of a node at a given depth
     pub fn get_size(&self, depth: usize) -> f32 {
         let depth = depth as f32;
@@ -191,39 +230,37 @@ impl TreeSetter for LODSetter {
         stack.push((root, Vec2::ZERO, 1));
 
         while let Some((parent, parent_center, parent_depth)) = stack.pop() {
-            if parent_depth > self.max_depth {
+            let parent_size = self.get_size(parent_depth);
+            let parent_distance = parent_center.length();
+            // Check if the node should be subdivided
+            if parent_size <= self.distance_to_size(parent_distance) {
                 continue;
             }
 
-            // Check if the node should be subdivided
-            let parent_distance = parent_center.length();
-            if self.should_subdivide(parent_depth as u16, parent_distance) {
-                let depth = parent_depth + 1;
-                let size = self.get_size(depth);
-                let size_half = size / 2.0;
-                // Subdivide the node
-                let children: [Box<MapChunkNode>; 4] = QUAD_TREE_DIRECTIONS
-                    .iter()
-                    .map(|direction| {
-                        let center = parent_center + *direction * size_half;
-                        Box::new(MapChunkNode::new_leaf(center, size, depth))
-                    })
-                    .collect_vec()
-                    .try_into()
-                    .unwrap();
+            let depth = parent_depth + 1;
+            let size = self.get_size(depth);
+            let size_half = size / 2.0;
+            // Subdivide the node
+            let children: [Box<MapChunkNode>; 4] = QUAD_TREE_DIRECTIONS
+                .iter()
+                .map(|direction| {
+                    let center = parent_center + *direction * size_half;
+                    Box::new(MapChunkNode::new_leaf(center, size, depth))
+                })
+                .collect_vec()
+                .try_into()
+                .unwrap();
 
-                *parent = MapChunkNode::Split(children);
+            *parent = MapChunkNode::Split(children);
 
-                if let MapChunkNode::Split(children) = parent {
-                    for child in children {
-                        let (pos, depth) = if let MapChunkNode::Data(ref data) = **child {
-                            (data.pos, data.depth)
-                        } else {
-                            panic!()
-                        };
-
-                        stack.push((child, pos, depth));
-                    }
+            if let MapChunkNode::Split(children) = parent {
+                for child in children {
+                    let (pos, depth) = if let MapChunkNode::Data(ref data) = **child {
+                        (data.pos, data.depth)
+                    } else {
+                        panic!()
+                    };
+                    stack.push((child, pos, depth));
                 }
             }
         }
@@ -238,14 +275,6 @@ struct ChunkHeights([u8; CHUNK_ITEM_COUNT]);
 
 #[derive(Debug, Component)]
 struct RenderChunkLink(pub Vec<Entity>);
-
-#[derive(Debug, Clone, Copy, Component, PartialEq, Eq)]
-struct IChunkPos(pub i32, pub i32);
-#[derive(Debug, Clone, Copy, Component, PartialEq)]
-struct FChunkPos(pub f32, pub f32);
-
-#[derive(Debug, Resource)]
-struct ChunkViewDistance(u16);
 
 #[derive(Debug, Resource)]
 struct MaxHeight(u16);
@@ -266,12 +295,6 @@ struct Height(u8);
 impl Height {
     pub fn to_world_y(&self) -> f32 {
         self.0 as f32 / 5.0
-    }
-}
-
-impl From<IChunkPos> for FChunkPos {
-    fn from(value: IChunkPos) -> Self {
-        Self(value.0 as f32, value.1 as f32)
     }
 }
 
@@ -307,28 +330,21 @@ fn draw_gizmos(mut gizmos: Gizmos) {
 
 fn render_chunks(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     mut map_tree: ResMut<MapLOD>,
+    asset_server: Res<AssetServer>,
     max_height: Res<MaxHeight>,
-    view_distance: Res<ChunkViewDistance>,
+    chunk_setter: Res<LODSetter>,
 ) {
     // let mesh = asset_server
     //     .get_id_handle(MeshType::Plane.into())
     //     .unwrap_or_else(|| asset_server.add(Plane3d::new(*Direction3d::Y).into()));
 
-    let setter = LODSetter {
-        view_distance: view_distance.0,
-        ref_distance: view_distance.0 / 4,
-        min_precision: 2,
-        ref_precision: 4,
-        max_depth: 4,
-    };
-    map_tree.0.update_with_setter(&setter);
+    map_tree.0.update_with_setter(chunk_setter.as_ref());
 
     info!("chunks: {}", map_tree.0.iter().collect_vec().len());
 
     let material = asset_server.add(Color::DARK_GREEN.into());
-    for chunk in map_tree.0.iter() {
+    for chunk in map_tree.0.iter_mut() {
         let transform = Transform::from_xyz(chunk.pos.x, 0.0, chunk.pos.y);
 
         info!("depth {} -> size {}", chunk.depth, chunk.size);
@@ -341,7 +357,7 @@ fn render_chunks(
                 PbrBundle {
                     mesh: asset_server.add(create_subdivided_plane(
                         chunk.size,
-                        &ChunkHeights::default(),
+                        &chunk_heights,
                         CHUNK_SLICES,
                     )),
                     material: material.clone(),
@@ -350,6 +366,8 @@ fn render_chunks(
                 },
             ))
             .id();
+
+        chunk.entity = Some(render_id);
     }
 
     // for x in -5..5 {
@@ -377,15 +395,16 @@ fn render_chunks(
     // }
 }
 
-fn generate_height_chunk(max_height: &Res<MaxHeight>, pos: &Vec2, size: f32) -> ChunkHeights {
+fn generate_height_chunk(max_height: &Res<MaxHeight>, offset: &Vec2, size: f32) -> ChunkHeights {
     let mut chunk_heights = ChunkHeights::default();
     let max_index = CHUNK_SLICES as f32 - 1.0;
     for z in 0..CHUNK_SLICES {
         for x in 0..CHUNK_SLICES {
-            // 0.0 - 1.0
-            let sub_chunk_pos = FChunkPos(x as f32 / max_index * size, z as f32 / max_index * size);
-
-            let height_f = simplex_noise_2d_seeded(vec2(pos.x, pos.y), 0.0) / 2.0 + 0.5;
+            let pos = Vec2::new(
+                x as f32 / max_index * size + offset.x,
+                z as f32 / max_index * size + offset.y,
+            );
+            let height_f = simplex_noise_2d_seeded(pos, 0.0) / 2.0 + 0.5;
 
             // 0..max
             let height = (max_height.as_f32() * height_f) as u8;
@@ -478,10 +497,8 @@ mod tests {
     fn map_lod_precision() {
         let tree_creator = LODSetter {
             view_distance: 100,
-            ref_distance: 50,
-            min_precision: 2,
-            ref_precision: 4,
-            max_depth: 4,
+            max_size: 100.0,
+            min_size: 5.0,
         };
 
         let distances = vec![0.0, 1.0, 5.0, 10.0, 20.0, 50.0, 100.0, 500.0, 1000.0];
@@ -497,8 +514,6 @@ mod tests {
                     precision,
                     last.0,
                     last.1,
-										distance, tree_creator.ref_distance,
-                    distance / tree_creator.ref_distance as f32,
                 );
             }
             last_precision = Some((distance, precision));
@@ -510,10 +525,8 @@ mod tests {
         let mut tree = MapLOD::new();
         let setter = LODSetter {
             view_distance: 100,
-            ref_distance: 50,
-            min_precision: 2,
-            ref_precision: 4,
-            max_depth: 4,
+            max_size: 100.0,
+            min_size: 5.0,
         };
         tree.0.update_with_setter(&setter);
 
