f4b35dedfa9559019d274890e0cbae9613f40887
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-06-07 15:09:18 +0200

    adjust spawning of chunks

---
+++ src/render/map.rs
@@ -0,0 +1,293 @@
+use crate::utils::tree_node;
+use bevy::{math::*, prelude::*, render::extract_resource::ExtractResource};
+use bevy_easings::Lerp;
+use itertools::Itertools;
+
+#[derive(Debug, Component)]
+pub struct MapChunk;
+
+#[derive(Debug, Clone)]
+pub struct MapChunkData {
+    pub entity: Option<Entity>,
+    pub pos: Vec2,
+    pub size: f32,
+    pub precision: u8,
+}
+
+impl MapChunkData {
+    pub fn new(pos: Vec2, size: f32, precision: u8) -> Self {
+        Self {
+            entity: None,
+            pos,
+            size,
+            precision,
+        }
+    }
+}
+
+pub type MapChunkNode = tree_node::QuadTree<MapChunkData>;
+
+impl MapChunkNode {
+    pub fn new_leaf(pos: Vec2, size: f32, precision: u8) -> Self {
+        Self::Data(MapChunkData::new(pos, size, precision))
+    }
+    pub fn root(size: f32) -> Self {
+        Self::Data(MapChunkData::new(Vec2::ZERO, size, 1))
+    }
+}
+
+#[derive(Debug, Resource, Default)]
+pub struct MapLOD {
+    pub leafs: Box<Vec<MapChunkData>>,
+    pub min_size: f32,
+}
+impl MapLOD {
+    pub fn from_setter(setter: &LODSetter) -> Self {
+        let mut node = MapChunkNode::root(setter.view_distance.into());
+        node.update_with_setter(setter);
+        let leafs = node.iter().cloned().collect_vec();
+        Self {
+            min_size: leafs
+                .iter()
+                .min_by(|a, b| a.size.total_cmp(&b.size))
+                .map(|ch| ch.size)
+                .unwrap_or_default(),
+            leafs: Box::new(leafs),
+        }
+    }
+}
+
+#[derive(Resource, Debug, Clone, Default, ExtractResource, Reflect)]
+#[reflect(Resource)]
+pub struct LODSetter {
+    pub view_distance: u16,
+    pub factor_distance: u8,
+    pub base_precision: u8,
+}
+
+impl LODSetter {
+    pub fn distance_to_precision(&self, distance: f32) -> u8 {
+        let ref_distance = self.view_distance as f32 * self.factor_distance as f32 / 128.0;
+        let linear_factor = (ref_distance - distance.min(ref_distance)) / ref_distance;
+        let factor = linear_factor * linear_factor;
+
+        1_u8.lerp(&self.base_precision.min(10), &factor)
+    }
+
+    pub fn get_size(&self, precision: u8) -> f32 {
+        let depth = (precision - 1) as f32;
+
+        self.view_distance as f32 / (2.0_f32.powf(depth))
+    }
+
+    pub fn new(view_distance: u16, factor_distance: u8, base_precision: u8) -> Self {
+        Self {
+            view_distance,
+            factor_distance,
+            base_precision,
+        }
+    }
+}
+
+impl tree_node::TreeSetter for LODSetter {
+    type TreeNode = MapChunkNode;
+
+    fn handle_node(&self, root: &mut Self::TreeNode) {
+        let mut stack = Vec::with_capacity(16);
+        stack.push((root, Vec2::ZERO, 1));
+
+        while let Some((parent, parent_center, parent_depth)) = stack.pop() {
+            let parent_size = self.get_size(parent_depth);
+            let parent_distance = parent_center.length();
+
+            // Check if the node should not be split
+            if parent_depth >= self.distance_to_precision(parent_distance) {
+                // If so, then update the leaf
+                *parent = MapChunkNode::new_leaf(parent_center, parent_size, parent_depth);
+
+                continue;
+            }
+
+            let depth = parent_depth + 1;
+            let size = self.get_size(depth);
+            let size_half = size / 2.0;
+
+            // Check if it is already split
+            if let MapChunkNode::Split(children) = parent {
+                // Traverse
+                for (i, child) in children.iter_mut().enumerate() {
+                    let center = parent_center + tree_node::QUAD_TREE_DIRECTIONS[i] * size_half;
+
+                    stack.push((child, center, depth));
+                }
+
+                continue;
+            }
+
+            // If not, then subdivide the node
+            let children: [Box<MapChunkNode>; 4] = tree_node::QUAD_TREE_DIRECTIONS
+                .iter()
+                .map(|direction| {
+                    let center = parent_center + *direction * size_half;
+                    Box::new(MapChunkNode::new_leaf(center, size, depth))
+                })
+                .collect_vec()
+                .try_into()
+                .unwrap();
+
+            *parent = MapChunkNode::Split(children);
+
+            // Traverse
+            if let MapChunkNode::Split(children) = parent {
+                for (i, child) in children.iter_mut().enumerate() {
+                    let center = parent_center + tree_node::QUAD_TREE_DIRECTIONS[i] * size_half;
+                    stack.push((child, center, depth));
+                }
+            };
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn map_lod_decreases() {
+        let tree_creator = LODSetter::new(100, 100, 5);
+
+        let distances = vec![0.0, 1.0, 5.0, 10.0, 20.0, 50.0, 100.0, 500.0, 1000.0];
+        let mut last_precision: Option<(f32, u16)> = None;
+
+        for distance in distances {
+            let precision = tree_creator.distance_to_precision(distance);
+            if let Some(last) = last_precision {
+                assert!(
+                    u16::from(precision) <= last.1,
+                    "precision shouldnt be higher: (distance: {}, precision: {}) > ({}, {})",
+                    distance,
+                    precision,
+                    last.0,
+                    last.1,
+                );
+            }
+            last_precision = Some((distance, precision.into()));
+        }
+    }
+
+    #[test]
+    fn map_lod_create() {
+        let setter = LODSetter::new(100, 100, 5);
+        let tree = MapLOD::from_setter(&setter);
+
+        let leafs = tree
+            .leafs
+            .iter()
+            .sorted_unstable_by(|a, b| a.pos.length().total_cmp(&b.pos.length()))
+            .collect_vec();
+
+        let min_size = leafs
+            .iter()
+            .min_by(|a, b| a.size.total_cmp(&b.size))
+            .unwrap()
+            .size;
+        let max_size = leafs
+            .iter()
+            .max_by(|a, b| a.size.total_cmp(&b.size))
+            .unwrap()
+            .size;
+
+        let min_distance = leafs
+            .iter()
+            .min_by(|a, b| a.pos.length().total_cmp(&b.pos.length()))
+            .unwrap()
+            .pos
+            .length();
+
+        let max_distance = leafs
+            .iter()
+            .max_by(|a, b| a.pos.length().total_cmp(&b.pos.length()))
+            .unwrap()
+            .pos
+            .length();
+
+        println!("Total chunks {}", leafs.len());
+
+        let leaf_data_str = leafs
+            .iter()
+            .map(|leaf| {
+                format!(
+                    "{} [{}] -> {}",
+                    leaf.pos.length(),
+                    leaf.precision,
+                    leaf.size
+                )
+            })
+            .join("\n");
+
+        assert_ne!(min_size, max_size);
+        assert_ne!(min_distance, max_distance);
+
+        for leaf1 in &leafs {
+            for leaf2 in &leafs {
+                let pos_ordering = leaf1.pos.length().total_cmp(&leaf2.pos.length());
+                let size_ordering = leaf1.size.total_cmp(&leaf2.size);
+                if size_ordering.is_ne() {
+                    assert!(
+                        pos_ordering == size_ordering,
+                        "Expected size to be smaller with smaller distance. Received: \n{}",
+                        leaf_data_str
+                    );
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_distance_to_precision() {
+        let setter = LODSetter::new(100, 50, 4);
+
+        // Test when distance is equal to reference distance
+        assert_eq!(setter.distance_to_precision(50.0), 4);
+
+        // Test when distance is less than reference distance
+        assert_eq!(setter.distance_to_precision(25.0), 16);
+
+        // Test when distance is greater than reference distance
+        assert_eq!(setter.distance_to_precision(75.0), 4);
+
+        // Test when distance is greater than view distance
+        assert_eq!(setter.distance_to_precision(150.0), 4);
+    }
+
+    #[test]
+    fn test_get_size() {
+        let setter = LODSetter::new(100, 50, 4);
+
+        // Test precision 1
+        assert_eq!(setter.get_size(1), 100.0);
+
+        // Test precision 2
+        assert_eq!(setter.get_size(2), 50.0);
+
+        // Test precision 3
+        assert_eq!(setter.get_size(3), 25.0);
+
+        // Test precision 4
+        assert_eq!(setter.get_size(4), 12.5);
+    }
+
+    #[test]
+    fn test_lod_chunk_min_size() {
+        let setter = LODSetter::new(100, 100, 5);
+        let tree = MapLOD::from_setter(&setter);
+        let min_size = tree
+            .leafs
+            .iter()
+            .min_by(|a, b| a.size.total_cmp(&b.size))
+            .unwrap()
+            .size;
+
+        assert_eq!(tree.min_size, min_size);
+    }
+}
---
+++ src/utils/tree_node.rs
@@ -0,0 +1,124 @@
+use bevy::prelude::*;
+
+pub const CHUNK_SLICES: usize = 2;
+pub const CHUNK_ITEM_COUNT: usize = CHUNK_SLICES * CHUNK_SLICES;
+
+pub const QUAD_TREE_DIRECTIONS: [Vec2; 4] = [
+    Vec2::new(-1.0, 1.0),
+    Vec2::new(1.0, 1.0),
+    Vec2::new(-1.0, -1.0),
+    Vec2::new(1.0, -1.0),
+];
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum FixedTreeNode<const SIZE: usize, T> {
+    Data(T),
+    Split([Box<FixedTreeNode<SIZE, T>>; SIZE]),
+}
+
+pub trait TreeSetter {
+    type TreeNode;
+
+    fn handle_node(&self, root: &mut Self::TreeNode);
+}
+
+impl<const SIZE: usize, T> FixedTreeNode<SIZE, T> {
+    pub fn is_leaf(&self) -> bool {
+        match self {
+            FixedTreeNode::Data(_) => true,
+            FixedTreeNode::Split(_) => false,
+        }
+    }
+
+    pub fn iter(&self) -> FixedTreeNodeIterator<'_, SIZE, T> {
+        FixedTreeNodeIterator { stack: vec![self] }
+    }
+
+    pub fn iter_mut(&mut self) -> FixedTreeNodeIteratorMut<'_, SIZE, T> {
+        FixedTreeNodeIteratorMut { stack: vec![self] }
+    }
+
+    pub fn update_with_setter<S: TreeSetter<TreeNode = Self>>(&mut self, setter: &S) {
+        setter.handle_node(self);
+    }
+}
+
+pub struct FixedTreeNodeIterator<'a, const SIZE: usize, T> {
+    pub stack: Vec<&'a FixedTreeNode<SIZE, T>>,
+}
+
+pub struct FixedTreeNodeIteratorMut<'a, const SIZE: usize, T> {
+    pub stack: Vec<&'a mut FixedTreeNode<SIZE, T>>,
+}
+
+impl<'a, const SIZE: usize, T> Iterator for FixedTreeNodeIterator<'a, SIZE, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let node = match self.stack.pop() {
+                Some(n) => n,
+                None => return None,
+            };
+
+            match node {
+                FixedTreeNode::Data(ref data) => return Some(data),
+                FixedTreeNode::Split(ref children) => {
+                    for child in children.iter() {
+                        self.stack.push(child);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'a, const SIZE: usize, T> Iterator for FixedTreeNodeIteratorMut<'a, SIZE, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let node = match self.stack.pop() {
+                Some(n) => n,
+                None => return None,
+            };
+
+            match node {
+                FixedTreeNode::Data(ref mut data) => return Some(data),
+                FixedTreeNode::Split(ref mut children) => {
+                    for child in children.iter_mut() {
+                        self.stack.push(child);
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub type OcTree<T: Sized> = FixedTreeNode<8, T>;
+
+pub type QuadTree<T: Sized> = FixedTreeNode<4, T>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::Itertools;
+
+    #[test]
+    fn tree_traverse() {
+        let tree: FixedTreeNode<4, i32> = FixedTreeNode::Split([
+            Box::new(FixedTreeNode::Data(0)),
+            Box::new(FixedTreeNode::Data(1)),
+            Box::new(FixedTreeNode::Data(2)),
+            Box::new(FixedTreeNode::Split([
+                Box::new(FixedTreeNode::Data(3)),
+                Box::new(FixedTreeNode::Data(4)),
+                Box::new(FixedTreeNode::Data(5)),
+                Box::new(FixedTreeNode::Data(6)),
+            ])),
+        ]);
+        for (i, node) in tree.iter().collect_vec().iter().rev().enumerate() {
+            assert_eq!(i as i32, **node);
+        }
+    }
+}
