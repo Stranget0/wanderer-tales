8cb5b73f39691802f682ec7d59fe7efd8a4856c1
Marcin Smarzewski <m.smarzewski.ts@gmail.com>
2024-08-11 15:53:26 +0200

    add shader load log

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -4,6 +4,7 @@ use bevy::{
     dev_tools::states::log_transitions,
     pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
+    render::render_resource::ShaderImport,
 };
 
 use crate::{game::CameraOrbit, screen::Screen};
@@ -20,7 +21,12 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, log_transitions::<Screen>);
     app.add_systems(
         Update,
-        (add_forward_gizmo, add_world_gizmo, add_camera_debug),
+        (
+            add_forward_gizmo,
+            add_world_gizmo,
+            add_camera_debug,
+            log_shader_load,
+        ),
     );
 }
 
@@ -62,5 +68,38 @@ fn add_directions_gizmos(gizmos: &mut Gizmos, start: Vec3, rotation: Quat) {
     ];
     for (dir, color) in g {
         gizmos.arrow(start, start + (rotation * dir), color.with_alpha(0.3));
+    }
+}
+
+fn log_shader_load(mut asset_event: EventReader<AssetEvent<Shader>>, shaders: Res<Assets<Shader>>) {
+    for event in asset_event.read() {
+        let e = match event {
+            AssetEvent::Added { id } => {
+                let shader = shaders.get(*id).unwrap();
+                Some(("Added", shader))
+            }
+            AssetEvent::Unused { id } => {
+                let shader = shaders.get(*id).unwrap();
+                Some(("Unused", shader))
+            }
+            AssetEvent::Removed { id } => {
+                let shader = shaders.get(*id).unwrap();
+                Some(("Reoved", shader))
+            }
+            _ => None,
+        };
+        let Some((action, path, import_path)) = e.and_then(|e| {
+            let ShaderImport::Custom(import_path) = e.1.import_path() else {
+                return None;
+            };
+            if import_path.contains("wanderer_tales") {
+                return Some((e.0, &e.1.path, import_path));
+            }
+            None
+        }) else {
+            continue;
+        };
+
+        info!("{} shader {} => {}", action, path, import_path);
     }
 }
